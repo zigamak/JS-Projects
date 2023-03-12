cb6eeb43fd5cc0fecde28e18beef6c3717d65297
zigamak <zigamakinde@gmail.com>
2023-03-12 20:06:22 +0100

    function addEventListener

--- 01_color_flipper/app.js
+++ 01_color_flipper/app.js
@@ -6,4 +6,6 @@ const color= document.querySelector('color');
 btn.addEventListener("click", function(){
     //get random number between 0 - 3
     const randomNumber = 2;
+    document.body.style.backgroundColor = colors [randomNumber];
+    color.textContent = colors[randomNumber]
 });
